1a3a5a7543343218a18aa801e897381b46c33bba
Juliano0195 <juliano.macedo@aluno.faculdadeimpacta.com.br>
2019-03-07 19:02:05 -0300

    Aulas

---
+++ aulas6.py
@@ -0,0 +1,18 @@
+joao = {'nome':'Joao', 'email':'vita@gmail.com', 'cel':988887777}
+maria = {'nome':'Maria', 'email':'mara@gmail.com', 'cel':988887777}
+jose = {'nome':'Jose', 'email':'vita@gmail.com'}
+
+#print(joao,maria,jose)
+
+lista_contatos = [{'nome':'Joao', 'email':'vita@gmail.com', 'cel':988887777},{'nome':'Maria', 'email':'mara@gmail.com', 'cel':988887777},
+{'nome':'Jose', 'email':'vita@gmail.com'}]
+
+lista_contatos = {'joao':{'nome':'Joao', 'email':'vita@gmail.com', 'cel':988887777},
+'maria': {'nome':'Maria', 'email':'mara@gmail.com', 'cel':988887777},
+'jose': {'nome':'Jose', 'email':'vita@gmail.com'}
+
+print(lista_contatos)
+
+#chave int or str
+
+print(hello)
